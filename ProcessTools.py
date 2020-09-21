e8f62fc1d08570ce7a2b9aeed2da7c76c1f48179
Jason Steffener NCMLab2 <steffener@gmail.com>
2020-09-21 15:43:24 -0400

    Update ProcessTools.py

--- ProcessTools.py
+++ ProcessTools.py
@@ -22,53 +22,88 @@ from scipy.stats import norm
 # Change sample sizes N: 10:10:200 (20)
 # TOTAL SIM = 160,000
 
-# Change A to be dichotomous with equal group size and redo
+
+# A is dichotomous with equal group size and redo
 # TOTAL SIM = 8,000
 
-# Change A to be dichotomous with unequal group size and redo
-# Group size ratio: 4, 3, 2, 0.5, 0.33, 0.25
+# A is dichotomous with unequal group size and redo
+# Group size ratio: 4, 3, 2, 0.5, 0.33, 0.25 (6)
+
+# A is uniformly distributed (1)
+
+# The covariance between A and B varies
+# 0:0.1:0.9 (10)
+
 # TOTAL SIM = 48,000
+#
+# plt.figure()
+# plt.hist(betalist[:,0])
+# plt.hist(betalist[:,1])
+# 
+# sns.pairplot(pd.DataFrame(data))
 
-
-
-plt.figure()
-plt.hist(betalist[:,0])
-plt.hist(betalist[:,1])
-
-sns.pairplot(pd.DataFrame(data))
-
-NSimMC = 1000
-NBoot = 2000
+NSimMC = 10
+NBoot = 200
 
 N = 100
 alpha = 0.05
 
-means = [1,1,1]
+means3 = [1,1,1]
+means4 = [1,1,1,1]
 Cov = 0.25
 covs = [[1,Cov,Cov],[Cov,1,Cov],[Cov,Cov,1]]
 Cov12 = 0.25
 Cov13 = 0.25
+Cov14 = 0.25
 Cov23 = 0.25
-covs = [[1,Cov12,Cov13],[Cov12,1,Cov23],[Cov13,Cov23,1]]
+Cov24 = 0.25
+Cov23 = 0.25
+Cov34 = 0.25
+covs3 = [[1,Cov12,Cov13],[Cov12,1,Cov23],[Cov13,Cov23,1]]
+covs4 = [[1,Cov12,Cov13,Cov14],[Cov12,1,Cov23, Cov24],[Cov13,Cov23,1,Cov34],[Cov14,Cov24,Cov34,1]]
 
-data = MakeMultiVariableData(N,means, covs)
-np.corrcoef(data.T)
+# Make data  where the DV is the last column (3) and a covariate is in column 2
+data = MakeMultiVariableData(N,means3, covs3)
+
+# Add the moderator
+data = MakeModeratedEffect(data,0,1, effect = 100)
+
+
+data = MakeMultiVariableData(N,[1, 1], [[1,0.99],[0.99,1]])
+print(np.corrcoef(data.T))
+Calculate_Beta_Sklearn(data)
+
+
+data = MakeIndependentData(N, means = [0,0,0], stdev = [1,1,1], weights = [1, 0.5, 0])
+print(np.corrcoef(data.T))
+data.mean(0)
+Calculate_Beta_Sklearn(data)
+
+
 # Point estimates
-PointEstimate2 = Calculate_Beta_Sklearn(data[:,[0,1]])
-PointEstimate3 = Calculate_Beta_Sklearn(data)
-# Point estimate mediation effects
-IE, TE, DE, a, b = CalculateMediationPEEffect(PointEstimate2, PointEstimate3)
+ai = 0
+bi = 1
+COVi = 2
+yi = 3
+# The last column is the DV
+PointEstimate2 = Calculate_Beta_Sklearn(data[:,[ai,COVi, bi]])
+# The last column is the DV
+PointEstimate3 = Calculate_Beta_Sklearn(data[:,[ai,bi,COVi,yi]])
+
+# Point estimate mediation effects and identify which effects to multiply
+IE, TE, DE, a, b = CalculateMediationPEEffect(PointEstimate2, PointEstimate3, 0, 1)
 
 # Bootstrap model 2
-BSbetalist2, BSinterlist2 = Resample_Sklearn(NBoot,Calculate_Beta_Sklearn, data[:,[0,1]])
-JKbetalist2, JKinterlist2 = JackKnife(Calculate_Beta_Sklearn, data[:,[0,1]])
+BSbetalist2, BSinterlist2 = Bootstrap_Sklearn(NBoot,Calculate_Beta_Sklearn, data[:,[ai,COVi, bi]])
+JKbetalist2, JKinterlist2 = JackKnife(Calculate_Beta_Sklearn, data[:,[ai,COVi, bi]])
 # Bootstrap model 3
-BSbetalist3, BSinterlist3 = Resample_Sklearn(NBoot,Calculate_Beta_Sklearn, data)
-JKbetalist3, JKinterlist3 = JackKnife(Calculate_Beta_Sklearn, data)
+BSbetalist3, BSinterlist3 = Bootstrap_Sklearn(NBoot,Calculate_Beta_Sklearn, data[:,[ai,bi,COVi,yi]])
+JKbetalist3, JKinterlist3 = JackKnife(Calculate_Beta_Sklearn, data[:,[ai,bi,COVi,yi]])
+
 # Bootstrap mediation effects
-BSIE, BSTE, BSDE, BSa, BSb = CalculateMediationResampleEffect(BSbetalist2, BSbetalist3)
+BSIE, BSTE, BSDE, BSa, BSb = CalculateMediationResampleEffect(BSbetalist2, BSbetalist3, 0, 1)
 # Jackknifemediation effects
-JKIE, JKTE, JKDE, JKa, JKb = CalculateMediationResampleEffect(JKbetalist2, JKbetalist3)
+JKIE, JKTE, JKDE, JKa, JKb = CalculateMediationResampleEffect(JKbetalist2, JKbetalist3, 0 , 1)
 
 
 IECI = CalculateBCaCI(BSIE, JKIE, IE, alpha)
@@ -76,6 +111,9 @@ TECI = CalculateBCaCI(BSTE, JKTE, TE, alpha)
 DECI = CalculateBCaCI(BSDE, JKDE, DE, alpha)
 aCI = CalculateBCaCI(BSa, JKa, a, alpha)
 bCI = CalculateBCaCI(BSb, JKb, b, alpha)
+
+# modCI = CalculateBCaCI(BSbetalist3[:,-1], JKbetalist3[:,-1], PointEstimate3[0][-1], alpha)
+
 print("a: %0.3f (%0.3f : %0.3f)"%(a,aCI[0],aCI[1]))
 print("b: %0.3f (%0.3f : %0.3f)"%(b,bCI[0],bCI[1]))
 print("TE: %0.3f (%0.3f : %0.3f)"%(TE,TECI[0],TECI[1]))
@@ -91,6 +129,25 @@ MClist = CalculatePower(NSimMC, NBoot, N, alpha, means, covs)
 MClist.sum(0)/NSimMC
 
 # Keep covs
+
+def MakeModeratedEffect(data,i = 0, j = 1, effect = 0):
+    # How big is the data
+    N,M = data.shape
+    # Remove mean of each column
+    iD = data[:,i] - data[:,i].mean()
+    print(iD.mean())
+    jD = data[:,j] - data[:,j].mean()
+    print(jD.mean())
+    # Create the moderation regressor. 
+    # This regressor is a vector and is converted to an array for the multiplication
+    mD = np.array(iD*jD)
+    print(mD.mean())
+    # Add a moderator effect to the DV
+    OutData = np.append(data, np.expand_dims(mD, axis = 1), axis = 1)
+    OutData[:,-1] = OutData[:,-1] + mD*effect
+    # Append the moderator to the data 
+    return OutData
+
 
 def SetupAllSims():
     NSimMC = 20
@@ -109,20 +166,20 @@ def SetupAllSims():
                     print(MClist.sum(0)/NSimMC)
                     
                     
-def CalculateMediationPEEffect(PointEstimate2, PointEstimate3):
+def CalculateMediationPEEffect(PointEstimate2, PointEstimate3, ia = 0, ib = 1):
     # Indirect effect
-    a = PointEstimate2[0][0]
-    b = PointEstimate3[0][1]
+    a = PointEstimate2[0][ia]
+    b = PointEstimate3[0][ib]
     IE = a*b
     # Direct effect
     DE = PointEstimate3[0][0]
     TE = DE + IE
     return IE, TE, DE, a, b
     
-def CalculateMediationResampleEffect(BSbetalist2, BSbetalist3):
+def CalculateMediationResampleEffect(BSbetalist2, BSbetalist3, ia, ib):
     # Indirect effect
-    a = np.squeeze(BSbetalist2)
-    b = np.squeeze(BSbetalist3[:,1])
+    a = np.squeeze(BSbetalist2[:,ia])
+    b = np.squeeze(BSbetalist3[:,ib])
     IE = a*b
     DE = np.squeeze(BSbetalist3[:,0])
     TE = DE + IE
@@ -209,15 +266,31 @@ def MakeMultiVariableData(N = 1000, means = [1,1,1], covs = [[1,0,0],[0,1,0],[0,
     x = np.random.multivariate_normal(means, covs, N)
     return x
 
+def MakeIndependentData(N = 1000, means = [0,0,0], stdev = [1,1,1], weights = [0, 0, 0]):
+    # Make sure everything is the correct size
+    M = len(means)
+    S = len(stdev)
+    W = len(weights)
+    if (M == S) and (M == W):
+        data = np.zeros([N,M])
+        # Create independent data
+        for i in range(M):
+            data[:,i] = np.random.normal(means[i], stdev[i], N)
+    # Add weights between predictors to DV
+    for i in range(M-1):
+        data[:,-1] = ddd= data[:,-1] + (data[:,i])*weights[i]
+    return data
+
+
 def Calculate_Beta_Sklearn(data):
     # using linear regression model from sklearn 
     lm = linear_model.LinearRegression()
     M = data.shape[1]
     #fit the x,y to the model,x will be a 2d matrix and y is a array
-    if M > 2:
-        model = lm.fit(data[:,[0,-2]], data[:,-1])
-    else:
-        model = lm.fit(data[:,[0]], data[:,-1])
+    # if M > 2:
+    model = lm.fit(data[:,0:-1], data[:,-1])
+    # else:
+    #     model = lm.fit(data[:,[0]], data[:,-1])
     return model.coef_, model.intercept_
 
 def Bootstrap_Sklearn(NBoot, func, data):
@@ -234,3 +307,18 @@ def Bootstrap_Sklearn(NBoot, func, data):
         interlist[i] = temp[1]
     return betalist,interlist
 
+def MakeData(
+def MakeIndependentData(N = 1000, means = [0,0,0], stdev = [1,1,1], weights = [0, 0, 0]):
+    # Make sure everything is the correct size
+    M = len(means)
+    S = len(stdev)
+    W = len(weights)
+    if (M == S) and (M == W):
+        data = np.zeros([N,M])
+        # Create independent data
+        for i in range(M):
+            data[:,i] = np.random.normal(means[i], stdev[i], N)
+    # Add weights between predictors to DV
+    for i in range(M-1):
+        data[:,-1] = ddd= data[:,-1] + (data[:,i])*weights[i]
+    return data
