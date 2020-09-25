176c4bc6ee874c8ae21ec11702dbc33d6ce6e078
Jason Steffener NCMLab2 <steffener@gmail.com>
2020-09-25 16:53:35 -0400

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+   
+def main():
+    for i in  range(100):
+        print("Hello %d"%(i))
+        
+if __name__ == "__main__":
+    main()
