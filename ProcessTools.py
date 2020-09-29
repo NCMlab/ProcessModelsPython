c7152fdffdc474ff774a4052caaa5d1ad67fa74b
Jason Steffener NCMLab2 <steffener@gmail.com>
2020-09-28 21:23:39 -0400

    Update ProcessTools.py

--- ProcessTools.py
+++ ProcessTools.py
@@ -490,17 +490,17 @@ def MakeBatchScripts():
                 for i9 in AtoC:
                     for i10 in BtoC:
                         count += 1
-                        if count == 1:
+                        #if count == 1:
                             # Create the script file
-                            f = open("submit_Process.sh", "w")
-                            f.write("#!/bin/bash\n")                        
-                            f.write("#SBATCH --time=06:00:00\n")
-                            f.write("#SBATCH --account=def-steffejr-ab\n")
-                            f.write("#SBATCH --mem-per-cpu=1024M\n")
-                            f.write("python ProcessTools.py %d %d %d %0.2f %0.2f %0.2f %d\n" %(1000,1000,i1,i8,i9,i10,i3))
-                            f.close()
-                            # submit the file to the queue
-                            os.system('sbatch submit_Process.sh')
+                        f = open("submit_Process.sh", "w")
+                        f.write("#!/bin/bash\n")                        
+                        f.write("#SBATCH --time=06:00:00\n")
+                        f.write("#SBATCH --account=def-steffejr-ab\n")
+                        f.write("#SBATCH --mem-per-cpu=512M\n")
+                        f.write("python ProcessTools.py %d %d %d %0.2f %0.2f %0.2f %d\n" %(1000,1000,i1,i8,i9,i10,i3))
+                        f.close()
+                        # submit the file to the queue
+                        os.system('sbatch submit_Process.sh')
                             
 
 def main():
