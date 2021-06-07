969ea448a9a774b926830045c3540ec9c641c9bf
Jason Steffener - laptop <steffener@gmail.com>
2021-06-07 15:16:25 -0400

    Update MakeScriptsv2.py

--- process_models/MakeScriptsv2.py
+++ process_models/MakeScriptsv2.py
@@ -24,61 +24,64 @@ def MakeBatchScripts():
     NSims = (7**8)*NSamples.shape[0]
 
     cNames = ['NBoot','NPower','N']
-    #for i in range(NParams):
-    for i in range(10000):
+    for i in range(NParams):
         cNames.append("param%03d"%(i+1))
-        cNames.append('SimID')
-        cNames.append('Completed')
+    cNames.append('SimID')
+    cNames.append('Completed')
+    #
+    
+
     
     
-        dfOut = pd.DataFrame(columns=cNames)
+    dfOut = pd.DataFrame(columns=cNames)
     
-        BaseDir = "/home/steffejr/Data002"
-        BaseDir ="/Users/jasonsteffener/Documents/TEMP"
-        #OutDir = '/Users/jasonsteffener/Documents/GitHub'
-        CodeDir = "/home/steffejr/scratch/ProcessModelsPython/process_models"
-        SubmissionListFileName = os.path.join(BaseDir,'SubmissionList.csv')    
-        count = 0
-        for i in Params:
-            count += 1
-            if count > -1:
-                # Create parameters as input
-                b1 = i[0]
-                b2 = i[1]
-                b3 = i[2]
-                b4 = i[3]
-                b5 = i[4]
-                b6 = i[5]
-                b7 = i[6]
-                b8 = i[7]            
-                # Create the script file
-                SimID = '%06d'%(count)
-                fileName = "submit_Process_%s"%(SimID)
-                f = open(os.path.join(BaseDir, 'jobs', fileName+".sh"), "w")
-                f.write("#!/bin/bash\n")                 
-                f.write("#SBATCH --job-name=%s.job\n"%(os.path.join(BaseDir, 'jobs', fileName)))
-                f.write("#SBATCH --output=%s.out\n"%(os.path.join(BaseDir, 'out', fileName)))
-                f.write("#SBATCH --error=%s.err\n"%(os.path.join(BaseDir, 'out', fileName)))
-                f.write("#SBATCH --time=06:00:00\n")
-                f.write("#SBATCH --account=def-steffejr-ab\n")
-                f.write("#SBATCH --mem-per-cpu=256M\n\n")
-                # Added an array for at least one dimension of simulations
-                f.write("#SBATCH --array=1-10\n")
-                f.write("source ~/ENV/bin/activate\n")
-                f.write("python %s %d %d %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f %s %s\n" %(os.path.join(CodeDir, "ModMedTools.py"), NBoot,NPower,b1,b2,b3,b4,b5,b6,b7,b8, os.path.join(BaseDir, 'out'), '$SLURM_ARRAY_TASK_ID'))
-                f.close()
-                # Add this sim to the dataframe keeping track
-                for j in NSamples:
-                    li = [NBoot, NPower,j,b1,b2,b3,b4,b5,b6,b7,b8,SimID, 0]
-                    row = pd.Series(li, index = cNames)
-                    dfOut = dfOut.append(row, ignore_index = True)
-                                
-                                # submit the file to the queue
-                                # os.system('sbatch %s'%(os.path.join(BaseDir, 'jobs', fileName+".sh")))
-                                # time.sleep(0.1)
-        print("Saving submission list to file: %s"%SubmissionListFileName)
-        dfOut.to_csv(SubmissionListFileName)
-        print(count)
+    BaseDir = "/home/steffejr/Data002"
+    BaseDir ="/Users/jasonsteffener/Documents/TEMP"
+    #OutDir = '/Users/jasonsteffener/Documents/GitHub'
+    CodeDir = "/home/steffejr/scratch/ProcessModelsPython/process_models"
+    SubmissionListFileName = os.path.join(BaseDir,'SubmissionList.csv')    
+    count = 0
+    # for i in Params:
+    for i in range(10000):
+        count += 1
+        if count > -1:
+            # Create parameters as input
+            b1 = i[0]
+            b2 = i[1]
+            b3 = i[2]
+            b4 = i[3]
+            b5 = i[4]
+            b6 = i[5]
+            b7 = i[6]
+            b8 = i[7]            
+            # Create the script file
+            SimID = '%06d'%(count)
+            fileName = "submit_Process_%s"%(SimID)
+            f = open(os.path.join(BaseDir, 'jobs', fileName+".sh"), "w")
+            f.write("#!/bin/bash\n")                 
+            f.write("#SBATCH --job-name=%s.job\n"%(os.path.join(BaseDir, 'jobs', fileName)))
+            f.write("#SBATCH --output=%s.out\n"%(os.path.join(BaseDir, 'out', fileName)))
+            f.write("#SBATCH --error=%s.err\n"%(os.path.join(BaseDir, 'out', fileName)))
+            f.write("#SBATCH --time=06:00:00\n")
+            f.write("#SBATCH --account=def-steffejr-ab\n")
+            f.write("#SBATCH --mem-per-cpu=256M\n\n")
+            # Added an array for at least one dimension of simulations
+            f.write("#SBATCH --array=1-10\n")
+            f.write("source ~/ENV/bin/activate\n")
+            f.write("python %s %d %d %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f %s %s\n" %(os.path.join(CodeDir, "ModMedTools.py"), NBoot,NPower,b1,b2,b3,b4,b5,b6,b7,b8, os.path.join(BaseDir, 'out'), '$SLURM_ARRAY_TASK_ID'))
+            f.close()
+            # Add this sim to the dataframe keeping track
+            for j in NSamples:
+                li = [NBoot, NPower,j,b1,b2,b3,b4,b5,b6,b7,b8,SimID, 0]
+                row = pd.Series(li, index = cNames)
+                dfOut = dfOut.append(row, ignore_index = True)
+                            
+                            # submit the file to the queue
+                            # os.system('sbatch %s'%(os.path.join(BaseDir, 'jobs', fileName+".sh")))
+                            # time.sleep(0.1)
+    print("Saving submission list to file: %s"%SubmissionListFileName)
+    dfOut.to_csv(SubmissionListFileName)
+    print(count)
 
 if __name__ == "__main__":
     MakeBatchScripts()
